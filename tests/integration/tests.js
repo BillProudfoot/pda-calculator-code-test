8a94bb4eb3e1ecb7fd442c8f25cdfa03431e7c56
Bill Proudfoot <billproudfoot@Bills-MacBook-Pro.local>
2017-07-04 00:20:47 +0100

    divide by zero tests added

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -35,8 +35,69 @@ describe('calculator functionality', function() {
     element(by.css('#operator_equals')).click();
     element(by.css('#operator_multiply')).click();
     element(by.css('#number2')).click();
-
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('can chain multiple operation together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number1')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1')
+  })
+
+  it('does it work with negative numbers - no it does not ', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1')
+  })
+
+  it('does it work subtracting to a negative numbers - no it does not', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number5')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
+  })
+
+  it('does it give a decimal answer - no it does not gives 2 ', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#clear')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('can it multiple very large numbers together - no it doesn not', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5000')
+  })
+
+  it('does it do if you divide by zero ', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number4')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0')
   })
 
 });
