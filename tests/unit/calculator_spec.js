3412592c7a03d114e89dbb92616cfdbd00f1ec4f
Bill Proudfoot <billproudfoot@Bills-MacBook-Pro.local>
2017-07-03 21:01:49 +0100

    multiply 3x5 test added and passes

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -11,4 +11,30 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  it('it should start at zero', function (){
+    assert.equal(null, this.previousTotal)
+  })
+
+  it('it should be able to multiply 3x5', function(){
+    calculator.previousTotal = 3;
+    calculator.multiply(5)
+    assert.equal(calculator.runningTotal, 15)
+  })
+
+  // it("add", function(){
+  //    assert.equal("2", calculator.add(1, 1));
+  //  });
+
+
+  // it('returns the sum of the numbers', function() {
+  //      expect(add(3,5)).this.runningTotal(8);
+  //    });
+
+//   it('returns the sum of the numbers', function() {
+//   expect(Calculator.add(number1, number2)).toBe(number1 + number2);
+// });
+
+
+  
+
 });
