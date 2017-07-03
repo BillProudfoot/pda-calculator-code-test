830b29390fd95017423732a5f71fa8c72e5e9833
Bill Proudfoot <billproudfoot@Bills-MacBook-Pro.local>
2017-07-03 22:53:10 +0100

    buttons, operations integration tests added

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,8 +16,27 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
-  it('should add two numbers', function (){
-    
+  it('running total should be displayed when number buttons are pressed', function (){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number6')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('123456')
+  })
+
+  it('use each operad and display total', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_equals')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
 });
